e8feefa2b791ad7ba5d0615acf9e1632f7c8c075
baptistegoncalves <146066736+baptistegoncalves@users.noreply.github.com>
2023-10-15 22:59:38 +0200

    search bar functional, login popup:bg-gray-200

--- script.js
+++ script.js
@@ -5,10 +5,36 @@ const body = document.getElementById("body")
 
 openBtn.addEventListener("click", function(){
     dialog.setAttribute("open", true)
-    body.classList.add("bg-red-500")
+    body.classList.add("bg-gray-200")
 })
 
 closeBtn.addEventListener("click", function(){
     dialog.removeAttribute("open")
-    body.classList.remove("bg-red-500")
+    body.classList.remove("bg-gray-200")
+});
+
+
+
+
+
+const searchForm = document.querySelector("form"); // Sélectionne le formulaire
+const searchInput = document.querySelector("input[type='text']"); // Sélectionne l'élément d'entrée de recherche
+const paragraphs = document.querySelectorAll("h3"); // Sélectionne toutes les balises <p> dans le document
+
+// Événement de soumission du formulaire
+searchForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Empêche la soumission du formulaire par défaut
+
+    const searchTerm = searchInput.value.trim(); // Obtient la valeur de la recherche
+
+    // Parcourir toutes les balises <p>
+    for (const paragraph of paragraphs) {
+    const paragraphText = paragraph.textContent.trim();
+
+    if (paragraphText === searchTerm) {
+        console.log(`"${searchTerm}" trouvé dans la page"`);
+    } else {
+        console.log(`"${searchTerm}" non trouvé dans la page"`);
+    }
+    }
 });
